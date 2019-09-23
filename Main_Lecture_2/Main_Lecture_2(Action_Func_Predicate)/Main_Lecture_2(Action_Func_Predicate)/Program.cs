c8bf1d4c821c6f10a8f71572a653131c34e46ec5
Kateryna Shynkarenko <kshynkarenko@intellias.com>
2019-09-23 16:38:05 +0300

    update methods + fix cycle

--- Main_Lecture_2/Main_Lecture_2(Action_Func_Predicate)/Main_Lecture_2(Action_Func_Predicate)/Program.cs
+++ Main_Lecture_2/Main_Lecture_2(Action_Func_Predicate)/Main_Lecture_2(Action_Func_Predicate)/Program.cs
@@ -8,12 +8,11 @@ namespace Main_Lecture_2_Action_Func_Predicate_
 {
     class Program
     {
+        static StringCollector collectionWithChars = new StringCollector();
+        static AlphaNumbericCollector collectionWithNambers = new AlphaNumbericCollector();
+
         static void Main(string[] args)
         {
-
-            StringCollector collectionWithChars = new StringCollector();
-            AlphaNumbericCollector collectionWithNambers = new AlphaNumbericCollector();
-
             Console.WriteLine("Do you want to add some string? y/n");
             string answer = Console.ReadLine();
             answer = answer.ToUpper();
@@ -23,7 +22,7 @@ namespace Main_Lecture_2_Action_Func_Predicate_
 
                 case "Y":
                     {
-                        Yeap();
+                        Yeap(answer);
                         break;
                     }
                 case "N":
@@ -33,64 +32,62 @@ namespace Main_Lecture_2_Action_Func_Predicate_
                     }
                 default:
                     {
-                        Default();
+                        Default(answer);
                         break;
                     }
             }
 
-            void Yeap()
-            {
-                while (answer == "Y")
-                {
-                    Console.WriteLine("Enter a string:\n");
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    string mes = Console.ReadLine();
-                    Console.ResetColor();
-
-                    Action<string> messageString = collectionWithChars.Actions;
-                    Action<string> messageNembers = collectionWithNambers.Actions;
-
-                    if (!mes.Any(c => char.IsDigit(c)))
-                    {
-                        messageString(mes);
-                    }
-                    else
-                    {
-                        messageNembers(mes);
-                    }
-                    Console.WriteLine("\nDo you want to add some string? y/n");
-
-                    answer = Console.ReadLine();
-                    answer = answer.ToUpper();
-
-                    if (answer == "N")
-                    {
-                        Noup();
-                    }
-
-                    else if (!(answer == "N" || answer == "Y"))
-                        Default();
-                }
-            }
-
-            void Noup()
-            {
-                Console.WriteLine("Have a nice evening. Bye");
-            }
-
-            void Default()
-            {
-                while (!(answer == "N" || answer == "Y"))
-                {
-
-                    Console.WriteLine("You type wrong answer. You should select y/n");
-                    answer = Console.ReadLine();
-                    answer = answer.ToUpper();
-                    Yeap();
-                }
-            }
-
             Console.ReadKey();
+        }
+
+        public static void Yeap(string _mes)
+        {
+            while (_mes == "Y")
+            {
+                Console.WriteLine("Enter a string:\n");
+                Console.ForegroundColor = ConsoleColor.Green;
+                string mes = Console.ReadLine();
+                Console.ResetColor();
+
+                Action<string> messageString = collectionWithChars.Actions;
+                Action<string> messageNembers = collectionWithNambers.Actions;
+
+                if (!mes.Any(c => char.IsDigit(c)))
+                {
+                    messageString(mes);
+                }
+                else
+                {
+                    messageNembers(mes);
+                }
+                Console.WriteLine("\nDo you want to add some string? y/n");
+
+                _mes = Console.ReadLine();
+                _mes = _mes.ToUpper();
+
+                if (_mes == "N")
+                {
+                    Noup();
+                }
+                else
+                    Default(_mes);
+            }
+        }
+
+        public static void Noup()
+        {
+            Console.WriteLine("Have a nice evening. Bye");
+        }
+
+        public static void Default(string _mes)
+        {  
+            while (!(_mes == "N" || _mes == "Y"))
+            {
+                Console.WriteLine("You type wrong answer. You should select y/n");
+                _mes = Console.ReadLine();
+                _mes = _mes.ToUpper();
+                Yeap(_mes);
+            }
         }
     }
 }
