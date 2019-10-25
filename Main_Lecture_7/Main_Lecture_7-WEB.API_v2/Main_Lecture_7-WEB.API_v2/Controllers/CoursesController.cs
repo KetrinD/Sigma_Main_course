74d21981546f11d65f9853d246da53fef52d508d
Kateryna Shynkarenko <kshynkarenko@intellias.com>
2019-10-25 15:04:40 +0300

    grr

---
+++ Main_Lecture_7/Main_Lecture_7-WEB.API_v2/Main_Lecture_7-WEB.API_v2/Controllers/CoursesController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Models.Models;
+
+namespace Web.Api.Demo.Controllers
+{
+    public class CoursesController : ControllerBase
+    {
+        private readonly Repository _repository;
+        private readonly IConfiguration _configuration;
+
+        public CoursesController(Repository repository, IConfiguration configuration)
+        {
+            _repository = repository;
+            _configuration = configuration;
+        }
+
+        //CreateCourse
+        [HttpPost]
+        public ActionResult<Course> CreateCourse([FromBody] Course course)
+        {
+            var newCourse = _repository.CreateCourse(course);
+            return newCourse;
+        }
+
+        //GetCourseById
+        [HttpGet]
+        public ActionResult<Course> GetCourseById ([FromRoute] int id)
+        {
+            var course = _repository.GetCourse(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return course;
+        }
+
+        //GetAllCourses
+        [HttpGet]
+        public ActionResult<List<Course>> GetAllCourses()
+        {
+            var courses = _repository.GetAllCourses();
+            return courses;
+        }
+
+        //UpdateCourseById
+        [HttpPut]
+        public ActionResult<Course> UpdateCourseById([FromBody]Course course)
+        {
+            _repository.UpdateCourse(course);
+            return course;
+        }
+
+        //DeleteCourseById
+        [HttpDelete]
+        public string DeleteCourseById([FromRoute] int id)
+        {
+            _repository.DeleteCourse(id);
+            return "ok";
+        }
+
+        //SetStudentsToCourse
+        [HttpPost]
+        [Route("Courses/SetStudentsToCourse/{courseId}")]
+        public Course SetStudentsToCourse([FromRoute] int courseId, [FromBody] IEnumerable<int> studentsId)
+        {
+            _repository.SetStudentsToCourse(courseId, studentsId);
+            var course = _repository.GetCourse(courseId);
+            return course;
+        }
+
+
+        //GetStudentsByCourseId
+        [HttpGet]
+        [Route("Courses/GetStudentsByCourseId/{courseId}")]
+        public ActionResult<List<Student>> GetStudentsByCourseId(int courseId)
+        {
+            List<Student> students = _repository.GetStudentsByCourseId(courseId);
+            return students;
+        }
+
+    }
+}
+
+
+
+////DeleteStudentsFromCourse
+//[HttpDelete]
+//[Route("Courses/DeleteStudentsFromCourse/{courseId}")]
+//public Course DeleteStudentsFromCourse([FromRoute] int courseId, IEnumerable<int> studentsId)
+//{
+//    _repository.DeleteStudentsFromCourse(courseId, studentsId);
+//    var course = _repository.GetCourse(courseId);
+//    return course;
+//}
+
---
+++ Main_Lecture_7/Main_Lecture_7-WEB.API_v2/Main_Lecture_7-WEB.API_v2/Controllers/StudentsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Models.Models;
+
+namespace Web.Api.Demo.Controllers
+{
+    public class StudentsController : ControllerBase
+    {
+        private readonly Repository _repository;
+        private readonly IConfiguration _configuration;
+
+        public StudentsController(Repository repository, IConfiguration configuration)
+        {
+            _repository = repository;
+            _configuration = configuration;
+        }
+
+        //CreateStudent
+        [HttpPost]
+        public ActionResult<Student> CreateStudent([FromBody] Student student)
+        {
+            var newStudent = _repository.CreateStudent(student);
+            return newStudent;
+        }
+
+        //GetStudentById
+        [HttpGet]
+        public ActionResult<Student> GetStudentById([FromRoute] int id)
+        {
+            var student = _repository.GetStudentById(id);
+
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return student;
+        }
+
+        //GetAllStudents
+        [HttpGet]
+        public ActionResult<List<Student>> GetAllStudents()
+        {
+            var students = _repository.GetAllStudents();
+            return students;
+        }
+
+        //UpdateStudent
+        [HttpPut]
+        public ActionResult<Student> UpdateStudentById([FromBody]Student student)
+        {
+            _repository.UpdateStudent(student);
+            return student;
+        }
+
+        //DeleteStudent
+        [HttpDelete]
+        public string DeleteStudentById([FromRoute] int id)
+        {
+            _repository.DeleteStudent(id);
+            return "ok";
+        }
+    }
+}
